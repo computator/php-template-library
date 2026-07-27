962a8145f3b7a02bf2a0b4ff32608a7719feaf24
Reuben Lifshay <rlifshay@gmail.com>
2026-07-26 20:35:53 -0700

    test: use TemplateRuntimeController::getConstructorTestArgs for tests

--- TemplateRuntimeController.php
+++ TemplateRuntimeController.php
@@ -3,6 +3,13 @@
 namespace Computator\FrameworkUtils\PHPTemplate;
 
 class TemplateRuntimeController {
+
+	public static function getConstructorTestArgs(\PHPUnit\Framework\TestCase $tc): array {
+		return (fn () => [
+			'renderer' => $this->createStub(Renderer::class),
+			'tpl' => $this->createStub(TemplateBase::class),
+		])->call($tc);
+	}
 	public function __construct(
 		public readonly Renderer $renderer,
 		public readonly TemplateBase $tpl,
