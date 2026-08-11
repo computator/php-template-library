f5ef332436a0377989591ccf06a092f93be2782a
Reuben Lifshay <rlifshay@gmail.com>
2026-08-10 23:49:55 -0700

    feat: add Buffer implementation

--- RenderTree/Buffer.php
+++ RenderTree/Buffer.php
@@ -3,5 +3,19 @@
 namespace Computator\FrameworkUtils\PHPTemplate\RenderTree;
 
 class Buffer implements Renderable {
-	protected string $buf = '';
+	public function __construct(
+		protected string $value = '',
+	) {}
+
+	public function getContents(): string {
+		return $this->value;
+	}
+
+	public function append(string $data): void {
+		$this->value .= $data;
+	}
+
+	public function render(): void {
+		echo $this->value;
+	}
 }
