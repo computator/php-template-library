2bc58aa76d0d89b1d280d4097333344dd90eaaac
Reuben Lifshay <rlifshay@gmail.com>
2026-08-13 21:56:46 -0700

    feat: update renderer to use RenderTree

--- Renderer.php
+++ Renderer.php
@@ -3,6 +3,7 @@
 namespace Computator\FrameworkUtils\PHPTemplate;
 
 use Computator\FrameworkUtils\PHPTemplate\Exceptions;
+use Computator\FrameworkUtils\PHPTemplate\RenderTree;
 use Computator\FrameworkUtils\PHPTemplate\Templates;
 
 use SplObjectStorage;
@@ -15,9 +16,15 @@ use function ob_get_level,ob_end_flush;
 class Renderer implements RenderManager, RenderClient {
 	protected readonly Templates\Base $root_template;
 	protected readonly TemplateResolver $resolver;
-	protected bool $rendering_to_string;
-	protected array $tpl_proxy_map = [];
 	protected SplObjectStorage $tpl_parent_map;
+	protected array $tpl_proxy_map = [];
+
+	protected readonly bool $using_tree;
+	protected RenderTree\Tree $rendertree;
+	protected ?RenderTree\Node $current_node = null;
+	protected ?RenderTree\Buffer $current_buff = null;
+
+	protected bool $rendering_to_string;
 
 	public static function create(Templates\Base $template, TemplateResolver $resolver = new TemplateResolver()): RenderClient {
 		return new self($template, $resolver);
@@ -28,22 +35,56 @@ class Renderer implements RenderManager, RenderClient {
 		$this->resolver = $resolver;
 
 		$this->tpl_parent_map = new SplObjectStorage();
+
+		// TODO: make using a tree optional once we can detect if
+		// there is any out of order rendering, or always use the tree
+		// if we can render the tree without any buffering
+		$this->initialize_tree();
 	}
 
 	public function render(): void {
 		$this->rendering_to_string = false;
-		$this->do_render($this->root_template);
+
+		// use try-finally since we want to send output as well
+		// as throw any errors
+		try {
+			$this->do_render($this->root_template);
+		} finally {
+			if ($this->using_tree)
+				$this->rendertree->render();
+		}
 	}
 
 	public function renderToString(): string {
 		$this->rendering_to_string = true;
+
 		ob_start();
+
 		$this->do_render($this->root_template);
+		if ($this->using_tree)
+			$this->rendertree->render();
+
 		return ob_get_clean();
 	}
 
+	protected function initialize_tree(): void {
+		$this->using_tree = true;
+		$this->rendertree = new RenderTree\Tree(
+			RenderTree\Node::withValue(null),
+		);
+	}
+
 	protected function do_render(Templates\Base $tpl): void {
+		if ($this->using_tree) {
+			if ($was_buffering = $this->current_buff !== null)
+				$this->current_buff->append(ob_get_clean());
+			$this->current_buff = new RenderTree\Buffer();
+			$this->rendertree->addValue($this->current_buff);
+			ob_start();
+		}
 		$start_ob_level = ob_get_level();
+		$error = null;
+
 		try {
 			// TODO: handle execute return values
 			$tpl->execute(
@@ -52,19 +93,34 @@ class Renderer implements RenderManager, RenderClient {
 				template: $tpl,
 			);
 		} catch (Throwable $t) {
-			throw new Exceptions\TemplateRenderException(
+			$error = new Exceptions\TemplateRenderException(
 				"error while rendering template: {$t->getMessage()}",
 				previous: $t,
 			);
-		} finally {
-			if (ob_get_level() > $start_ob_level) {
-				do {
-					ob_end_flush();
-				} while (ob_get_level() > $start_ob_level);
-
-				throw new Exceptions\TemplateRenderException("template did not close it's output buffers!");
-			}
 		}
+
+		if (ob_get_level() > $start_ob_level) {
+			do {
+				ob_end_flush();
+			} while (ob_get_level() > $start_ob_level);
+
+			if (!$error)
+				$error = new Exceptions\TemplateRenderException("template did not close it's output buffers!");
+		}
+
+		if ($this->using_tree) {
+			$this->current_buff->append(ob_get_clean());
+			if ($was_buffering) {
+				$this->current_buff = new RenderTree\Buffer();
+				$this->rendertree->addValue($this->current_buff);
+				ob_start();
+			}
+			else
+				$this->current_buff = null;
+		}
+
+		if ($error)
+			throw $error;
 	}
 
 	public function getTemplateAsProxy(string $template): RenderObjects\TemplateRenderProxy {
