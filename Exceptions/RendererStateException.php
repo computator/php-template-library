4e12061bd116065ba62d815a4bf17a605ab2d8f2
Reuben Lifshay <rlifshay@gmail.com>
2026-08-20 19:08:56 -0700

    chore: ignore coverage for RendererStateException

--- Exceptions/RendererStateException.php
+++ Exceptions/RendererStateException.php
@@ -5,6 +5,7 @@ namespace Computator\FrameworkUtils\PHPTemplate\Exceptions;
 use Throwable;
 
 class RendererStateException extends RendererException {
+	/** @codeCoverageIgnore */
 	public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null) {
 		parent::__construct(
 			"invalid state: {$message}",
